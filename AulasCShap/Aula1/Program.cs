5d7dd54e36bce24eb5649b4ce0b3dfd3501aa664
caiquecase <caique.souzacas@gmail.com>
2022-03-31 22:19:22 -0300

    Versão 0.0.1 - Aula de CSharp

---
+++ AulasCShap/Aula1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Estou chegando para ganhar 15k");
+        }
+    }
+}
